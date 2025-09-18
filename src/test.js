5b99bfc1d31ede77b5a09bac2c59c299a6af79ff
ArsenZar <arsenzar@gmail.com>
2025-09-18 14:17:48 +0300

    test pagination _limit = 5 and test _page

--- src/test.js
+++ src/test.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const fetchPosts = async () => {
-  const response = await axios.get("https://jsonplaceholder.typicode.com/posts?_limit=5");
+  const response = await axios.get("https://jsonplaceholder.typicode.com/posts?_limit=5&_page=3");
   console.log("Posts: ", response.data);
 };
+
 
 fetchPosts();
